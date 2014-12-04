c9a735bd13a68ae29993d9398e7c08f8d78c33d1
nbari <nbari@dalmp.com>
2014-12-04 18:01:20 +0100

    modified:   ftrunk.py 	modified:   restore.py

--- restore.py
+++ restore.py
@@ -1,7 +1,7 @@
 from base64 import b64decode
 from crypt import Crypt
 
-p = b64decode('=')
+p = b64decode('=i')
 x = Crypt(p)
 
 src = '/Users/nbari/.ftrunk/eb/c5/99/ebc5999af324a1f4272e15795690cb979dbea921a56fbd4957f5f83055bb7c04363d35bf475856637f0ff0929400507f1e998b7c709dae1de53748ab5f3381ca'
